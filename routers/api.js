387f5aed6dff6dcad930c2a26f3d053e69cceb9a
filecc <fil.gia@live.it>
2023-11-15 17:59:49 +0100

    Update post image and add double middleware for authentication and image upload

--- routers/api.js
+++ routers/api.js
@@ -4,12 +4,14 @@ const apiController = require("../controllers/api")
 const multer = require("multer")
 const authMiddleware = require("../middleware/auth");
 
+const doubleMiddleware = [authMiddleware, multer({dest: "public/images"}).single("image")]
+
 router.get("/posts", apiController.index)
 router.post('/login', apiController.login)
-router.post("/post/add", multer({dest: "public/images"}).single("image"), apiController.store)
+router.post("/post/add", doubleMiddleware, apiController.store)
 router.post("/delete/:id", authMiddleware, apiController.destroy)
 router.get("/post/:id", apiController.show)
-router.post("/edit", multer({dest: "public/images"}).single("image"), apiController.edit)
+router.post("/edit", doubleMiddleware, apiController.edit)
 
 
 
